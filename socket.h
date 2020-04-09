84321eeff20522d885c8a00083158fa0112d4f7b
Ben <bennguyen96@gmail.com>
2020-04-09 16:27:28 +1000

    attempt to refactor socket stuff

---
+++ socket.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <stdbool.h>
+#ifndef PORT_NO
+#define PORT_NO 80
+#endif
